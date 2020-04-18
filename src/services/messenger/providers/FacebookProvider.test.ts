790aa88430fc4b39ce92b115f2db6488bbf00bd1
Max Pijittum <mahasak@gmail.com>
2020-04-18 18:15:56 +0800

    fix json and test

--- src/services/messenger/providers/FacebookProvider.test.ts
+++ src/services/messenger/providers/FacebookProvider.test.ts
@@ -4,16 +4,34 @@ import fetch from "node-fetch";
 jest.mock("node-fetch");
 
 describe("OpenCageDataProvider", () => {
+  
     test("an empty query string", async () => {
-      (fetch as any).mockImplementation(() => {return {features: []}});
-      const result = await Provider.getPlaces("Paris");
+      const mockResponse = Promise.resolve({
+        ok: false,
+        status: 503,
+        json: () => {
+            return {features: []};
+        },
+       });
+       
+      (fetch as any).mockImplementation(() => {return mockResponse});
+      const result = await Provider.getPlaces("");
       expect(result).toEqual({ features: [] });
     });
+    
     /*
     test("an invalid non-json response", async () => {
-      (fetch as any).mockImplementation(() => "Service Unavailable.");
+
+      const mockResponse = Promise.resolve({
+        ok: false,
+        status: 503,
+        json: () => {
+            return Promise.reject(JSON.parse("Service Unavailable."));
+        },
+       });
+      (fetch as any).mockImplementation(() => mockResponse);
       const result = await Provider.getPlaces("Paris");
-      await expect(Provider.getPlaces("Chamonix")).resolves.toThrow(SyntaxError);
-    });
+      await expect(Provider.getPlaces("Chamonix")).rejects.toThrow(SyntaxError);
+    }); 
     */
-  });
+ });
--- src/services/messenger/providers/FacebookProvider.ts
+++ src/services/messenger/providers/FacebookProvider.ts
@@ -9,5 +9,5 @@ export const getPlaces = async (query: string) => {
   const url = `https://api.opencagedata.com/geocode/v1/geojson?q=${query}&key=${key}&limit=20&no_annotations=1`;
   
   const response = await fetch(url);
-  return response;
+  return await response.json();
 };
